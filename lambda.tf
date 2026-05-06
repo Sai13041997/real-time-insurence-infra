1c9beafdc927889f7bcd017b2cdda4d6274f97ab
Sai13041997 <praneethsattenapalli1310@gmail.com>
2026-05-06 19:55:07 -0400

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,15 +1,29 @@
 module "hello_lambda" {
-  source = "git::https://github.com/kfbmic/tf-module-lambda-function.git?ref=main"
-
-  name                   = "${var.application_name}-${var.env_tier}-hello-lambda"
-  runtime                = "python3.13"
-  handler                = "index.handler"
-  timeout                = 10
-  memory_size            = 128
+  source      = "git::https://github.com/kfbmic/tf-module-lambda-function.git?ref=v1.0.0"
+  name = "${var.application_name}-${var.env_tier}-hello-lambda"
+  runtime = "python3.13"
+  handler = "index.handler"
+  timeout     = 10
+  memory_size = 128
   code_managed_elsewhere = true
-  vpc_enabled            = true
-  vpc_id                 = var.vpc_id
-  subnet_ids             = var.lambda_subnet_ids
+  vpc_enabled = true
+  vpc_id      = var.vpc_id
+  subnet_ids  = var.lambda_subnet_ids
+
+  tags = {}
+}
+
+module "sample_lambda" {
+  source      = "git::https://github.com/kfbmic/tf-module-lambda-function.git?ref=v1.0.0"
+  name = "${var.application_name}-${var.env_tier}-sample-lambda"
+  runtime = "python3.13"
+  handler = "index.handler"
+  timeout     = 10
+  memory_size = 128
+  code_managed_elsewhere = true
+  vpc_enabled = true
+  vpc_id      = var.vpc_id
+  subnet_ids  = var.lambda_subnet_ids
 
   tags = {}
 }
