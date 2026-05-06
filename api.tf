f8a407d600545dade1f3929c462c8b3b1a5eeddd
Sai13041997 <praneethsattenapalli1310@gmail.com>
2026-05-06 07:20:17 -0400

    Create api.tf

---
+++ api.tf
@@ -0,0 +1,16 @@
+module "api_gateway" {
+  source = "git::https://github.com/kfbmic/tf-module-api-gateway.git?ref=main"
+
+  name          = "${var.application_name}-${var.env_tier}-api-gateway"
+  is_production = false
+
+  jwt_audience = ["${var.entra_audience}"]
+
+  routes = [
+    {
+      path       = "/hello"
+      method     = "GET"
+      lambda_arn = module.hello_lambda.lambda_function_arn
+    }
+  ]
+}
