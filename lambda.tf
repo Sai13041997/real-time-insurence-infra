1fe0576b7bc674696915a1df05ebbf8d88f9b823
Sai13041997 <praneethsattenapalli1310@gmail.com>
2026-05-06 07:21:05 -0400

    Create lambda.tf

---
+++ lambda.tf
@@ -0,0 +1,15 @@
+module "hello_lambda" {
+  source = "git::https://github.com/kfbmic/tf-module-lambda-function.git?ref=main"
+
+  name                   = "${var.application_name}-${var.env_tier}-hello-lambda"
+  runtime                = "python3.13"
+  handler                = "index.handler"
+  timeout                = 10
+  memory_size            = 128
+  code_managed_elsewhere = true
+  vpc_enabled            = true
+  vpc_id                 = var.vpc_id
+  subnet_ids             = var.lambda_subnet_ids
+
+  tags = {}
+}
